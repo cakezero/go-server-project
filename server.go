594daba61bcc50ce1714c26a6a7eaeb5c7e1d9a5
cakezero <sareahj.com@gmail.com>
2025-07-09 20:47:06 +0100

    server entrypoint

--- server.go
+++ server.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/cakezero/go-server/src/routes"
+	// "github.com/cakezero/go-server/src/utils"
+	"net/http"
+	"log"
+)
 
 
 func main() {
-	fmt.Println("Hello, World!")
+	routes.Routes()
+
+	// utils.GetRedisClient()
+
+	fmt.Println("Server is running on port: 9000")
+
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
 }
