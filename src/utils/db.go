238c7430929c429d9c17038022b6b6840a7550e1
cakezero <sareahj.com@gmail.com>
2025-07-13 21:38:09 +0100

    db setup to use mongodb  through mgm

--- src/utils/db.go
+++ src/utils/db.go
@@ -1,23 +1,26 @@
 package utils
 
 import (
-	// "fmt"
+	"github.com/kamva/mgm/v3"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/redis/go-redis/v9"
 )
 
 func GetRedisClient() *redis.Client {
-	err := LoadEnv()
-	if err != nil {
-		panic(err)
-	}
 
 	redisClient := redis.NewClient(&redis.Options{
-		Addr: DB_URI,
-		Username: DB_USERNAME,
-		Password: DB_PASSWORD,
+		Addr: REDIS_URI,
+		Username: REDIS_USERNAME,
+		Password: REDIS_PASSWORD,
 		DB: 0,
 	})
 
 	return redisClient
+}
+
+func DB () error {
+	err := mgm.SetDefaultConfig(nil, "go_calculator", options.Client().ApplyURI(DB_URI))
+
+	return err
 }
