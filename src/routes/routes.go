4391e6e121983e1206a626dd45b1b35c38335390
cakezero <sareahj.com@gmail.com>
2025-07-14 19:48:57 +0100

    middleware implementation on routes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,7 +1,10 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/cakezero/go-server/src/controllers"
+	"github.com/cakezero/go-server/src/middlewares"
 	"github.com/gorilla/mux"
 )
 
@@ -10,8 +13,8 @@ func Routes() *mux.Router {
 	router.HandleFunc("/", controllers.Home)
 	router.HandleFunc("/login", controllers.Login).Methods("POST")
 	router.HandleFunc("/register", controllers.Register).Methods("POST")
-	router.HandleFunc("/perform-action", controllers.PerformAction).Methods("POST")
-	router.HandleFunc("/get-history", controllers.ArithmeticHistory).Methods("GET")
+	router.Handle("/perform-action", middlewares.AuthMiddleware(http.HandlerFunc(controllers.PerformAction))).Methods("POST")
+	router.Handle("/get-history", middlewares.AuthMiddleware(http.HandlerFunc(controllers.ArithmeticHistory))).Methods("GET")
 
 	return router
 }
