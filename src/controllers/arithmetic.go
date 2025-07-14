912eae8f6687b95b69c342c49b7cd60a6271c0e0
cakezero <sareahj.com@gmail.com>
2025-07-14 19:45:28 +0100

    controller to use response fn from utils

--- src/controllers/arithmetic.go
+++ src/controllers/arithmetic.go
@@ -4,85 +4,68 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 	"net/http"
 	"strconv"
+
+	"github.com/cakezero/go-server/src/middlewares"
 	"github.com/cakezero/go-server/src/models"
+	"github.com/cakezero/go-server/src/utils"
 	"github.com/kamva/mgm/v3"
 	"go.mongodb.org/mongo-driver/bson"
-	"math"
 )
 
 var Ctx = context.Background()
 
-func Response (res http.ResponseWriter, responseMessage, status string, resData ...interface{}) {
-	switch status {
-	case "e":
-		res.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(res).Encode(GetJSONMessage("e", responseMessage))
-	case "b":
-		res.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(res).Encode(GetJSONMessage("e", responseMessage))
-	default:
-		if len(resData) > 0 {
-			data := GetJSONMessage("", responseMessage)
-			data["data"] = resData[0]
-			res.WriteHeader(http.StatusOK)
-			json.NewEncoder(res).Encode(data)
-		} else {
-			res.WriteHeader(http.StatusOK)
-			json.NewEncoder(res).Encode(GetJSONMessage("", responseMessage))
-		}
-	}
-}
-
 func ArithmeticHistory(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
 
-	id := req.URL.Query().Get("id")
+	id := req.Context().Value(middlewares.IdKey).(string)
+	fmt.Printf("id: %s\n", id)
 	var history []models.Arithmetic
 
 	historyFetch, findErr := mgm.Coll(&models.Arithmetic{}).Find(Ctx, bson.M{"user": id})
 	if findErr != nil {
 		fmt.Println(findErr.Error())
-		Response(res, "No arithmetic has been performed", "")
+		utils.Response(res, "No arithmetic has been performed", "")
 		return
 	}
 
 	if fetchErr := historyFetch.All(Ctx, &history); fetchErr != nil {
-		Response(res, "Error fetching history", "e")
+		utils.Response(res, "Error fetching history", "e")
 		return
 	}
 
-	Response(res, "arithmetic history fetched!", "", history)
+	utils.Response(res, "arithmetic history fetched!", "", history)
 }
 
 func PerformAction(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
-
+	id := req.Context().Value(middlewares.IdKey).(string)
+	fmt.Printf("id: %s\n", id)
 	var userArithmetic models.Arithmetic
 	type computation struct {
 		First string
 		Second string
 		Action string
-		Id string
 	}
 
 	var userAction computation
 
 	if decodeErr := json.NewDecoder(req.Body).Decode(&userAction); decodeErr != nil {
-		Response(res, "Data received from request is not appropriate", "e")
+		utils.Response(res, "Data received from request is not appropriate", "e")
 		return
 	}
 
 	firstNumber, firstErr := strconv.ParseFloat(userAction.First, 64)
 	if firstErr != nil {
-		Response(res, "Only integer strings are allowed", "b")
+		utils.Response(res, "Only integer strings are allowed", "b")
 		return
 	}
 
 	secondNumber, secondErr := strconv.ParseFloat(userAction.Second, 64)
 	if secondErr != nil {
-		Response(res, "Only integer strings are allowed", "b")
+		utils.Response(res, "Only integer strings are allowed", "b")
 		return
 	}
 
@@ -104,20 +87,20 @@ func PerformAction(res http.ResponseWriter, req *http.Request) {
 		answer = math.Round((firstNumber / secondNumber) * 1000) /1000
 		symbol = "➗"
 	default: 
-		Response(res, "Specify arithmetic action", "b")
+		utils.Response(res, "Specify arithmetic action", "b")
 		return
 	}
 
 	userArithmetic.Equation = fmt.Sprintf("%.0f %s %.0f 🟰 %.0f", firstNumber, symbol, secondNumber, answer)
 	userArithmetic.Action = action
-	userArithmetic.User = userAction.Id
+	userArithmetic.User = id
 
 	saveErr := mgm.Coll(&userArithmetic).Create(&userArithmetic)
 
 	if saveErr != nil {
-		Response(res, "Error saving arithmetic calculation", "e")
+		utils.Response(res, "Error saving arithmetic calculation", "e")
 		return
 	}
 
-	Response(res, "arithmetic action performed", "", answer)
+	utils.Response(res, "arithmetic action performed", "", answer)
 }
--- src/controllers/auth.go
+++ src/controllers/auth.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 
 	"github.com/cakezero/go-server/src/models"
+	"github.com/cakezero/go-server/src/utils"
 	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/go-playground/validator/v10"
@@ -15,18 +16,8 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type GlobalMap map[string]any
-
 var validate = validator.New()
 
-func GetJSONMessage(Type, message string) GlobalMap {
-	switch Type {
-		case "e":
-			return GlobalMap{"error": message}
-		default:
-			return GlobalMap{"message": message}
-	}
-}
 
 func Login(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("Login hit!")
@@ -35,19 +26,19 @@ func Login(res http.ResponseWriter, req *http.Request) {
 	var user models.User
 
 	if err := json.NewDecoder(req.Body).Decode(&user); err != nil {
-		Response(res, "Data received from request is not appropriate", "b")
+		utils.Response(res, "Data received from request is not appropriate", "b")
 		return
 	}
 
 	if user.Email == "" || user.Password == "" {
-		Response(res, "Both email and password are required", "b")
+		utils.Response(res, "Both email and password are required", "b")
 		return
 	}
 
 	userProp := mgm.Coll(&models.User{}).FindOne(context.Background(), bson.M{"email": user.Email})
 
 	if userProp.Err() != nil {
-		Response(res, "email or password is invalid", "b")
+		utils.Response(res, "email or password is invalid", "b")
 		return
 	}
 
@@ -55,16 +46,16 @@ func Login(res http.ResponseWriter, req *http.Request) {
 
 	decodeErr := userProp.Decode(&userFound)
 	if decodeErr != nil {
-		Response(res, "internal server error", "e")
+		utils.Response(res, "internal server error", "e")
 		return
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(userFound.Password), []byte(user.Password)); err != nil {
-		Response(res, "email or password is invalid", "b")
+		utils.Response(res, "email or password is invalid", "b")
 		return
 	}
 
-	Response(res, "user logged in", "", userFound)
+	utils.Response(res, "user logged in", "", userFound)
 }
 
 func Register(res http.ResponseWriter, req *http.Request) {
@@ -74,17 +65,17 @@ func Register(res http.ResponseWriter, req *http.Request) {
 	var user models.User
 
 	if err := json.NewDecoder(req.Body).Decode(&user); err != nil {
-		Response(res, "Request data received was not appropriate", "b")
+		utils.Response(res, "Request data received was not appropriate", "b")
 		return
 	}
 
 	if validateError := validate.Struct(user); validateError != nil {
-		Response(res, "All fields are required", "b")
+		utils.Response(res, "All fields are required", "b")
 		return
 	}
 
 	if length := len(user.Password); length < 8 {
-		Response(res, "Password must be gte 8", "b")
+		utils.Response(res, "Password must be gte 8", "b")
 		return
 	}
 
@@ -92,7 +83,7 @@ func Register(res http.ResponseWriter, req *http.Request) {
 	if checkUserErr := mgm.Coll(existingUser).First(bson.M{"email": user.Email}, existingUser); checkUserErr != nil {
 		hashedPassword, hashErr := bcrypt.GenerateFromPassword([]byte(user.Password), 12)
 		if hashErr != nil {
-			Response(res, "Internal server error", "e")
+			utils.Response(res, "Internal server error", "e")
 			return
 		}
 
@@ -100,15 +91,15 @@ func Register(res http.ResponseWriter, req *http.Request) {
 		saveErr := mgm.Coll(&user).Create(&user)
 
 		if saveErr != nil {
-			Response(res, "User not saved", "e")
+			utils.Response(res, "User not saved", "e")
 			return
 		}
 
-		Response(res, "User saved", "", user)
+		utils.Response(res, "User saved", "", user)
 		return
 	}
 
-	Response(res, "Email exists", "b")
+	utils.Response(res, "Email exists", "b")
 }
 
 func Home(res http.ResponseWriter, req *http.Request) {
