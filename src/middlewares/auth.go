b608818e6bf269cf1403583bd307625590a95dd4
cakezero <sareahj.com@gmail.com>
2025-07-15 02:27:49 +0100

    middleware to use redis db for caching

--- src/middlewares/auth.go
+++ src/middlewares/auth.go
@@ -2,14 +2,17 @@ package middlewares
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"strings"
-	"fmt"
+
 	"github.com/cakezero/go-server/src/utils"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/redis/go-redis/v9"
 )
 
 var IdKey = "user-id"
+var ctx = context.Background()
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
@@ -25,7 +28,16 @@ func AuthMiddleware(next http.Handler) http.Handler {
 			return
 		}
 
-		parsedToken, tokenParseErr := jwt.Parse(authParts[1], func(token *jwt.Token) (interface {}, error) {
+		accessToken := authParts[1]
+
+		revoked, redisErr := utils.GetRedisClient().Get(ctx, accessToken).Result()
+
+		if redisErr != redis.Nil || revoked == "revoked" {
+			utils.Response(res, "Access token has been revoked", "u")
+			return
+		}
+
+		parsedToken, tokenParseErr := jwt.Parse(accessToken, func(token *jwt.Token) (interface {}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, fmt.Errorf("unexpected signing method")	
 			}
