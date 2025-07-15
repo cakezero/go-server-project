a6073aa943cd794d4011635e5c0124aa0ec7bcf2
cakezero <sareahj.com@gmail.com>
2025-07-15 02:30:43 +0100

    chore: minor route fixes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -10,11 +10,12 @@ import (
 
 func Routes() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/", controllers.Home)
+	router.HandleFunc("/logout", controllers.Logout)
+	router.HandleFunc("/refresh", controllers.RefreshTokenHandler)
 	router.HandleFunc("/login", controllers.Login).Methods("POST")
 	router.HandleFunc("/register", controllers.Register).Methods("POST")
 	router.Handle("/perform-action", middlewares.AuthMiddleware(http.HandlerFunc(controllers.PerformAction))).Methods("POST")
-	router.Handle("/get-history", middlewares.AuthMiddleware(http.HandlerFunc(controllers.ArithmeticHistory))).Methods("GET")
+	router.Handle("/get-history", middlewares.AuthMiddleware(http.HandlerFunc(controllers.ArithmeticHistory)))
 
 	return router
 }
